a353a5057de7c1db20e5827e10cf001b41d35844
Guillermo García <guillermieres97@gmail.com>
2022-07-07 11:37:44 +0200

    fetching api

--- js/index.js
+++ js/index.js
@@ -9,23 +9,44 @@ async function fetchMoviesJson() {
 }
 
 fetchMoviesJson().then(data => {
-    const movies = data.peliculas;
+    const movies = data.movies;
+    const characters = data.characters;
     for (let index = 0; index < movies.length; index++) {
         const moviesSection = document.getElementById("moviesSection");
         /* let id = movies[index].id; */
         let image = movies[index].img;
         let title = movies[index].nombre;
-        let description = movies[index].descripción;
+        let director = movies[index].director;
+        let gender = movies[index].gender;
         moviesSection.innerHTML += `
         <div class="col">
-            <div class="card text-bg-dark h-100">
-                <img src="../img/camera.jpg" class="card-img-top" alt="...">
+            <div class="card text-bg-dark" style="height: 385px;">
                 <div class="card-body">
-                    <img src="${image}" class="card-img-top">
-                    <h5 class="card-title">${title}</h5>
+                    <img src="${image}" class="card-img-top" style="height: 210px;">
+                    <h5 class="card-title fw-bold">${title}</h5>
+                    <p class="card-text">${director}<br>${gender}</p>
+                    <a href="#" class="btn btn-dark btnHover">Watch Detail</a>
+            </div>
+        </div>
+        `;
+    }
+    for (let index = 0; index < characters.length; index++) {
+        const charactersSection = document.getElementById("charactersSection");
+        /* let id = movies[index].id; */
+        let image = characters[index].img;
+        let title = characters[index].nombre;
+        let description = characters[index].descripción;
+        charactersSection.innerHTML += `
+        <div class="col">
+            <div class="card text-bg-dark" style="height: 385px;">
+                <div class="card-body">
+                    <img src="${image}" class="card-img-top" style="height: 210px;">
+                    <h5 class="card-title fw-bold">${title}</h5>
                     <p class="card-text">${description}</p>
+                    <a href="#" class="btn btn-dark btnHover">Watch Detail</a>
             </div>
         </div>
         `;
     }
 })
+
